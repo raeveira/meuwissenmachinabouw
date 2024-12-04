71555d1abd3f75c139e3b777995477ad077c9241
rae <rae@raeveira.nl>
2024-12-04 21:45:57 +0100

    removed unused import

--- components/Front-Header.tsx
+++ components/Front-Header.tsx
@@ -1,7 +1,6 @@
 'use client'
 import Image from "next/image";
 import { Itim } from "next/font/google";
-import {Bookmark} from "lucide-react";
 import Link from "next/link";
 import {useRef, useState} from "react";
 
