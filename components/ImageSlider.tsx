2b3ec8afcfd5c14154130618fc6b5263023616c7
rae <rae@raeveira.nl>
2024-12-09 12:08:03 +0100

    created image slider and added track around field

---
+++ components/ImageSlider.tsx
@@ -0,0 +1,39 @@
+import React, {useEffect, useState} from 'react';
+
+const ImageSlider = ({images}: { images: string[] }) => {
+    const [isAnimated, setIsAnimated] = useState(false);
+    const [isClient, setIsClient] = useState(false);
+
+    useEffect(() => {
+        setIsClient(true);
+    }, []);
+
+    useEffect(() => {
+        if (!isClient) return;
+        setIsAnimated(true);
+    }, [isClient]);
+
+    return (
+        <div
+            className={`slider ${isAnimated ? 'overflow-hidden' : ''}`}
+        >
+            <div
+                className="flex items-center h-full animate-scroll"
+                style={{
+                    width: 'max-content',
+                }}
+            >
+                {[...images, ...images, ...images].map((image, index) => (
+                    <img
+                        key={index}
+                        src={image}
+                        alt={`Slide ${index + 1}`}
+                        className="flex-shrink-0 max-h-[39px]"
+                    />
+                ))}
+            </div>
+        </div>
+    );
+};
+
+export default ImageSlider;
